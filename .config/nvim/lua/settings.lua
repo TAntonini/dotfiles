8941c89dc938317519a464a4cee767d5caea48e0
Thomas Antonini <antonini.thomas@gmail.com>
2021-04-18 16:42:35 +0200

    nvim lua: Add terminal settings
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -15,3 +15,8 @@ vim.cmd('set smarttab')
 vim.cmd('set tabstop=4')            -- Number of characters for <Tab>
 vim.cmd('set termguicolors')        -- 24-bit colors
 vim.cmd('set updatetime=50')
+
+-- Terminal
+vim.cmd("let $GIT_EDITOR = 'nvr -cc split --remote-wait'")  -- Use nvr for avoiding nested nvim when using git from nvim terminal
+vim.cmd("autocmd FileType gitcommit,gitrebase,gitconfig set bufhidden=delete")  -- Permits to use :wq or :x instead of :w | bd
+vim.cmd("autocmd TermOpen * setlocal nonumber norelativenumber")    -- Hide line number in terminals
